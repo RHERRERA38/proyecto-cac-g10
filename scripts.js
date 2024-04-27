26fa8b073bccf403e0eeaeca1987169dfdd98a05
hugogoncalvez <hugogoncalvez@gmail.com>
2024-04-27 12:57:14 -0300

    menu-27-04
    se modifica el menú, los estilos y js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,40 @@
 const sidebarBtn = document.querySelector(".toggle-btn");
 const sidebar = document.querySelector("aside");
+const main = document.querySelector(".main-menu");
 
 sidebarBtn.addEventListener("click", () => {
   document.body.classList.toggle("active");
+  main.classList.toggle("siderbar-activate");
+});
+
+// Menu
+
+function mostrarDiv(idBoton) {
+
+  const idDiv = idBoton.replace('button-', '');
+
+  const divs = document.querySelectorAll('.div-carta');
+
+  divs.forEach(div => {
+    div.classList.remove('show')
+  })
+  const divMostrar = document.getElementById(idDiv);
+
+  divMostrar.classList.add('show');
+}
+
+
+const tabs = document.querySelectorAll('.a-tabs');
+
+tabs.forEach(tab => {
+
+  tab.addEventListener('click', function () {
+
+    tabs.forEach(t => {
+      t.classList.remove('active');
+    });
+    this.classList.add('active');
+
+    mostrarDiv(this.id);
+  });
 });
