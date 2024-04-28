a8c38f35ec7fdccfc3970afbb525393fbc35231c
ramon <ramonherrera1008@gmail.com>
2024-04-27 21:01:59 -0300

    Se separa los estilos del formulario en un nuevo archivo reserva.css. Se modifico elementos del formulario, se cambió radio buttons por lista deplegales para las sedes, se implementa checkbox para promociones. Se agrega logica de indicación de error con JavaScript

--- js/scripts.js
+++ js/scripts.js
@@ -2,5 +2,62 @@ const sidebarBtn = document.querySelector(".toggle-btn");
 const sidebar = document.querySelector("aside");
 
 sidebarBtn.addEventListener("click", () => {
-  document.body.classList.toggle("active");
+    document.body.classList.toggle("active");
 });
+
+// Instrucciones asociadas al formulario de reservas
+let btnReserva = document.querySelector('#btn-reserva');
+
+btnReserva.addEventListener('click', function() {
+    let name = document.querySelector('#name');
+    let email = document.querySelector('#email');
+    let telf = document.querySelector('#telf');
+    let cant = document.querySelector('#cant');
+    let date = document.querySelector('#date');
+    let time = document.querySelector('#time');
+    let sede = document.querySelector('#sedes');
+
+    if(name.value.trim()==''){
+        name.classList.add('error');
+        let errorname = document.querySelector('#error-name');
+        errorname.innerHTML = 'Debe completar el campo Nombre';
+    }
+
+    if(email.value.trim()==''){
+        email.classList.add('error');
+        let erroremail = document.querySelector('#error-email');
+        erroremail.innerHTML = 'Debe completar el campo email';
+    }
+
+    if(telf.value.trim()==''){
+        telf.classList.add('error');
+        let errortelf = document.querySelector('#error-telf');
+        errortelf.innerHTML = 'Debe completar el campo N° Teléfono';
+    }
+
+    if(cant.value.trim()==''){
+        cant.classList.add('error');
+        let errorcant = document.querySelector('#error-cant');
+        errorcant.innerHTML = 'Debe indicar la cantidad de comensales';
+    }
+
+    if(date.value.trim()==''){
+        date.classList.add('error');
+        let errordate = document.querySelector('#error-date');
+        errordate.innerHTML = 'Debe indicar la fecha de la reserva';
+    }
+
+    if(time.value.trim()==''){
+        time.classList.add('error');
+        let errortime = document.querySelector('#error-time');
+        errortime.innerHTML = 'Debe indicar hora de la reserva';
+    }
+
+    if(sede.value.trim()==''){
+        sede.classList.add('error');
+        let errorsede = document.querySelector('#error-sedes');
+        errorsede.innerHTML = 'Debe seleccionar una sede';
+    }
+
+
+})
