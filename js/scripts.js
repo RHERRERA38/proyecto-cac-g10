b35e5d992edee0747ed5be7ca4264d56dca12983
Ramón Herrera <ramonherrera1008@gmail.com>
2024-05-05 21:45:09 -0300

    Mejora en el diseño resposivo del formulario reservas. Barra Lateral en código de Javascript. Se ajusto colores y tamaño de logo en styles.css para barra lateral.

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,56 @@
+// Script Asociado a la barra Lateral 
+const navbar = `
+<nav>
+    <div class="sidebar-header">
+        <a class="logo-wrapper">
+            <img src="./assets/img/rest-logo.png" alt="Logo">
+            <h2 class="hidden">BISTRO</h2>
+        </a>
+        <button class="toggle-btn">
+            <img src="./assets/svg/expand.svg" alt="expand button">
+        </button>
+    </div>
+
+    <div class="sidebar-links">
+        <a class="link" href="index.html">
+            <img src="./assets/svg/home.svg" alt="">
+            <span class="hidden">Inicio</span>
+        </a>
+        <a class="link" href="menu.html">
+            <img src="./assets/svg/menu.svg" alt="">
+            <span class="hidden">Menu</span>
+        </a>
+        <a class="link" href="reservas.html">
+            <img src="./assets/svg/reservas.svg" alt="">
+            <span class="hidden">Reservas</span>
+        </a>
+        </li>
+        <a class="link" href="contacto.html">
+            <img src="./assets/svg/ubicacion.svg" alt="">
+            <span class="hidden">Ubicación y Contacto</span>
+        </a>
+    </div>
+</nav>`;
+document.querySelector("body").insertAdjacentHTML("afterbegin", navbar);
+
+
+
 const sidebarBtn = document.querySelector(".toggle-btn");
 const sidebar = document.querySelector("aside");
 
 sidebarBtn.addEventListener("click", () => {
     document.body.classList.toggle("active");
+    main.classList.toggle("siderbar-activate");
 });
 
+//Fin de Script asociado a la Barra lateral
+
 // Instrucciones asociadas al formulario de reservas
+
 let btnReserva = document.querySelector('#btn-reserva');
 
-btnReserva.addEventListener('click', function() {
+btnReserva.addEventListener('click', function(e) {
+
     let name = document.querySelector('#name');
     let email = document.querySelector('#email');
     let telf = document.querySelector('#telf');
@@ -16,6 +58,8 @@ btnReserva.addEventListener('click', function() {
     let date = document.querySelector('#date');
     let time = document.querySelector('#time');
     let sede = document.querySelector('#sedes');
+
+    e.preventDefault();
 
     if(name.value.trim()==''){
         name.classList.add('error');
