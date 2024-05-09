2a11f37327e9c7c60f76e0e456410c8c7d7e090e
Ramón Herrera <ramonherrera1008@gmail.com>
2024-05-09 19:49:08 -0300

    Se corrige error de JavaScript en boton reserva en reservas.html. Este error impedía ejecutar la acción submit del formulario.

--- js/scripts.js
+++ js/scripts.js
@@ -86,49 +86,59 @@ btnReserva.addEventListener('click', function(e) {
     let date = document.querySelector('#date');
     let time = document.querySelector('#time');
     let sede = document.querySelector('#sedes');
-
-    e.preventDefault();
+    let isEmpty = false;
 
     if(name.value.trim()==''){
         name.classList.add('error');
         let errorname = document.querySelector('#error-name');
         errorname.innerHTML = 'Debe completar el campo Nombre';
+        isEmpty = true;
     }
 
     if(email.value.trim()==''){
         email.classList.add('error');
         let erroremail = document.querySelector('#error-email');
         erroremail.innerHTML = 'Debe completar el campo email';
+        isEmpty = true;
     }
 
     if(telf.value.trim()==''){
         telf.classList.add('error');
         let errortelf = document.querySelector('#error-telf');
         errortelf.innerHTML = 'Debe completar el campo N° Teléfono';
+        isEmpty = true;
     }
 
     if(cant.value.trim()==''){
         cant.classList.add('error');
         let errorcant = document.querySelector('#error-cant');
         errorcant.innerHTML = 'Debe indicar la cantidad de comensales';
+        isEmpty = true;
     }
 
     if(date.value.trim()==''){
         date.classList.add('error');
         let errordate = document.querySelector('#error-date');
         errordate.innerHTML = 'Debe indicar la fecha de la reserva';
+        isEmpty = true;
     }
 
     if(time.value.trim()==''){
         time.classList.add('error');
         let errortime = document.querySelector('#error-time');
         errortime.innerHTML = 'Debe indicar hora de la reserva';
+        isEmpty = true;
     }
 
     if(sede.value.trim()==''){
         sede.classList.add('error');
         let errorsede = document.querySelector('#error-sedes');
         errorsede.innerHTML = 'Debe seleccionar una sede';
+        isEmpty = true;
+    }
+
+    if(isEmpty) {
+        e.preventDefault();
     }
 
 })
